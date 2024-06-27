0eb325adf229fee224e1b7124cedead1c4ecbe8e
David Kamayor <davidkamayor@gmail.com>
2024-06-27 18:11:16 -0400

    implement public functions

--- src/lib.rs
+++ src/lib.rs
@@ -94,6 +94,21 @@ impl Emu {
         self.execute(op);
     }
 
+    pub fn get_display(&self) -> &[bool] {
+        &self.screen
+    }
+
+    pub fn keypress(&mut self, idx: usize, pressed: bool) {
+        // idx must be less than 16
+        self.keys[idx] = pressed;
+    }
+
+    pub fn load(&mut self, data: &[u8]) {
+        let start = START_ADDR as usize;
+        let end = (START_ADDR as usize) + data.len();
+        self.ram[start..end].copy_from_slice(data);
+    }
+
     fn execute(&mut self, op: u16) {
         let digit1 = (op & 0xF000) >> 12;
         let digit2 = (op & 0x0F00) >> 8;
